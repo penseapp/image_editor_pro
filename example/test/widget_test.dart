a5eda6beae7c1cc9a471f218bcd1b2aded597c73
João Mantovani <jpm.mantovani45@gmail.com>
2021-07-17 23:23:05 -0300

    first commit

---
+++ example/test/widget_test.dart
@@ -0,0 +1,11 @@
+import 'package:firexcode/firexcode.dart';
+
+
+class Test extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
