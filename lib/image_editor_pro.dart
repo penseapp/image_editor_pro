5833111d64e381e8ea67664e47efdbd3528e9b45
João Mantovani <jpm.mantovani45@gmail.com>
2022-05-29 18:26:56 -0300

    chore: build for web

--- lib/image_editor_pro.dart
+++ lib/image_editor_pro.dart
@@ -22,7 +22,6 @@ import 'package:image_editor_pro/theme/colors.dart';
 import 'package:image_editor_pro/utils/offset_class.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:image_picker_web/image_picker_web.dart';
-// import 'package:image_picker_web/image_picker_web.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:screenshot/screenshot.dart';
